11532f33ed70350f02bf43f977b010f9252fde6d
Kaze-Kesh <115439258+kazekeshx@users.noreply.github.com>
2025-06-19 15:05:10 -0500

    finish2

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ const menuIcon = document.getElementById('openMenuBtn');
   menuIcon.addEventListener('click', () => {
     menu.classList.toggle('abierto');
   });
+
+  const header = document.getElementById('header');
+  const main= document.getElementById('main');
+  window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll > 0){
+    header.classList.add('active');
+    main.classList.add('active');
+  }
+  else{
+    header.classList.remove('active');
+    main.classList.remove('active');
+  }
+});
